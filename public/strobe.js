688776ab5dae41a8647971301d50c104c91833d5
Alex Anderson <alexandersonone@gmail.com>
2017-05-08 19:44:15 -0700

    Made the description a bit better

---
+++ public/strobe.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  document.getElementById("startbutton").onclick = function(){
+    document.getElementById("strobe").style.animationName = "strobe"
+    document.getElementById("hidemeafter").className = "hide"
+  };
+}
